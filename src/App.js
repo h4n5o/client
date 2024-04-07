4c212a14361053f66f120bc4b0c41eb983ea9673
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-04-07 15:52:03 +0200

    Streaming API start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+
+import React from 'react';
+import Header from './components/Header';
+import StreamList from './components/StreamList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <StreamList />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/StreamList.js
@@ -0,0 +1,30 @@
+// client/src/components/StreamList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const StreamList = () => {
+    const [streams, setStreams] = useState([]);
+
+    useEffect(() => {
+        // Hier anstelle von 'http://localhost:3001/api/streams' deine API-URL verwenden
+        axios.get("client/src/components/StreamList.js")
+            .then(response => {
+                setStreams(response.data);
+            })
+            .catch(error => {
+                console.error('Fehler beim Abrufen der Stream-Daten:', error);
+            });
+    }, []);
+
+    return (
+        <ul>
+            {streams.map(stream => (
+                <li key={stream.id}>
+                    <a href={stream.url}>{stream.title}</a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default StreamList;
